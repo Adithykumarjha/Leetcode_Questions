ff2c2cce5fb0ca10993f811a120c9e96035ca5fa
Adithy Kumar Jha <147861071+Adithykumarjha@users.noreply.github.com>
2026-03-08 16:17:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (16.51%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int L=0,R=0,count=0;
+
+        for(auto ch:s){
+            if(ch=='L'){
+                L++;
+            }
+            else{
+                R++;
+            }
+
+            if(L==R){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+};
