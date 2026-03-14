9c10e2869335e4e7ef991235602e3504488d7ad8
Adithy Kumar Jha <147861071+Adithykumarjha@users.noreply.github.com>
2026-03-14 09:47:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (76.42%) - LeetSync

---
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        int sum=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(t[j]==s[i]){
+                    sum+=abs(i-j);
+                    break;
+                }
+            }
+        }
+        return sum;
+        
+    }
+};
