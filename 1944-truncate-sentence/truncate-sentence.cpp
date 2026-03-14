cbc2969b890b0297e15dca2f2bf23aaa7061a872
Adithy Kumar Jha <147861071+Adithykumarjha@users.noreply.github.com>
2026-03-14 15:19:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (20.88%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]==' '&& --k==0){
+                return s.substr(0,i);
+            }
+        }
+        return s;
+
+        
+    }
+};
